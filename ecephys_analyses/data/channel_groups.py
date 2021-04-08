05cacc27e405448504bb5c1060012bae21f1914a
Graham Findlay <graham.b.findlay@gmail.com>
2021-04-08 18:03:49 -0500

    Added Allan and spontaneous wake for all animals

--- ecephys_analyses/data/channel_groups.py
+++ ecephys_analyses/data/channel_groups.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ecephys.neuropixels import checkerboard_map, long_column_map
 
 CheckPat = checkerboard_map.get_user_order()
@@ -9,6 +10,7 @@ full = {
     "Doppio": CheckPat,
     "Alessandro": LongCol,
     "Eugene": LongCol,
+    "Allan": np.concatenate([LongCol[41:], (LongCol[0:41])]),
 }
 
 # This list is incomplete
@@ -20,7 +22,9 @@ emg = {
     "Doppio": CheckPat[0:-1:191],  # "LF0;384", "LF383;767", "LF381;765" on imec0 (mpta)
     "Alessandro": LongCol[0:-1:191],  # "LF0;384", "LF382;766", "LF381;765"
     "Eugene": LongCol[0:-1:191],  # "LF0;384", "LF382;766", "LF381;765"
-    "Allan": [0, 328, 275],  # "LF0;384", "LF328;712", "LF275;659" on imec0 (frontal)
+    "Giuseppe": [10, 328, 275],  # LF10;394, LF328;712, LF275;659 on imec0
+    "Allan": [82, 75, 70],  # LF82;466, LF75;459, LF70;454 on imec1
+    # "Allan": [0, 328, 275],  # "LF0;384", "LF328;712", "LF275;659" on imec0 (frontal)
 }
 
 # Visually identified in the LFP during the first 2h of recovery sleep
@@ -30,6 +34,7 @@ stratum_pyrmidale_inversion = {
     "Doppio": [166],  # Maybe also 1 channel more dorsal
     "Alessandro": [165],  # Maybe also 1 channel more dorsal
     "Eugene": [203],
+    "Allan": [231],
 }
 
 superficial_ctx = {
@@ -38,6 +43,7 @@ superficial_ctx = {
     "Doppio": [374, 377, 378, 381, 382],
     "Alessandro": [375, 377, 379, 381, 383],
     "Eugene": [375, 377, 379, 381, 383],
+    "Allan": [72, 74, 76, 78, 80],
 }
 
 # Identified on the basis of whole-probe CSD from during recovery sleep.
@@ -47,24 +53,29 @@ hippocampus = {
     "Doppio": CheckPat[190:291],  # 3.8mm to 5.8mm from probe tip
     "Alessandro": LongCol[190:291],  # 3.8mm to 5.8mm from probe tip
     "Eugene": LongCol[240:311],  # 4.8mm to 6.2mm from probe tip
-}
-
-# All channels 140um to 200um ventral to the CA1 stratum pyramidale inversion.
-# Based on a fixed offset relative to the stratum pyrmidale channels, identified
-# during the first 2h of recvoery sleep.
-stratum_radiatum_140um_to_200um = {
-    "Segundo": [155, 157, 159, 161],
-    "Valentino": [165, 166, 169, 170],
-    "Doppio": [146, 149, 150, 153],
-    "Alessandro": [145, 147, 149, 151],
-    "Eugene": [183, 185, 187, 189],
+    "Allan": full["Allan"][175:291],  # 3.5mm to 5.8mm from probe tip
 }
 
 # Hippocampal channels, +/- additional channels above and below to allow for drift.
+# This is probably unnecessary, even for Valentino.
 drift_tracking = {
     "Segundo": LongCol[200:315],
     "Valentino": CheckPat[180:315],
     "Doppio": CheckPat[180:300],
     "Alessandro": LongCol[180:300],
     "Eugene": LongCol[230:320],
+    "Allan": full["Allan"][170:300],
+}
+
+# All channels 140um to 200um ventral to the CA1 stratum pyramidale inversion.
+# Based on a fixed offset relative to the stratum pyrmidale channels, identified
+# during the first 2h of recvoery sleep. Used for power calculation, not SPW
+# detection.
+stratum_radiatum_140um_to_200um = {
+    "Segundo": [155, 157, 159, 161],
+    "Valentino": [165, 166, 169, 170],
+    "Doppio": [146, 149, 150, 153],
+    "Alessandro": [145, 147, 149, 151],
+    "Eugene": [183, 185, 187, 189],
+    "Allan": [211, 213, 215, 217],
 }
