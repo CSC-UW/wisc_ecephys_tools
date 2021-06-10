386e2f485cdbc9402f1cfcf5cdaf2588e36124ad
Tom Bugnon <tombugnon@hotmail.com>
2021-06-10 08:43:27 -0500

    add ecephys_analyses/sorting

---
+++ ecephys_analyses/sorting/postprocessing.py
@@ -0,0 +1,141 @@
+from pathlib import Path
+from datetime import datetime
+import subprocess
+
+from ecephys_spike_sorting.scripts.create_input_json import createInputJson
+from ecephys_analyses.data import channel_groups, paths, parameters
+
+
+def run_postprocessing(
+    subject, condition,
+    sorting_condition, postprocessing_condition,
+    catgt_data=True,
+    rerun_existing=True
+):
+    
+    # Modules and associated parameters
+    modules = []
+    params = []
+    module_name_params = parameters.get_analysis_params(
+        'ks_postprocessing',
+       postprocessing_condition 
+    )
+    for d in module_name_params:
+        modules += d.keys()
+        params += d.values()
+    
+    # Data
+    assert catgt_data
+    binpaths = paths.get_sglx_style_datapaths(
+        subject, 
+        condition,
+        'ap.bin',
+        catgt_data=True,
+    )
+    assert len(binpaths) == 1
+    binpath = binpaths[0]
+    metapath = binpath.parent/(binpath.stem + '.meta')
+    print(f"catGT preprocessed data: {binpath}\n")
+    
+    # Src ks dir
+    ks_dir_src = paths.get_datapath(
+        subject,
+        condition,
+        sorting_condition,
+    )
+    assert ks_dir_src.exists()
+    print(f"Kilosort input results dir: {ks_dir_src}\n")
+
+    # Target
+    ks_dir = paths.get_datapath(
+        subject,
+        condition,
+        sorting_condition + '_' + postprocessing_condition,
+    )
+    copy_ks_dir(ks_dir_src, ks_dir)
+    
+    # Save config in ks dir
+    cfg_path = ks_dir/'postprocessing-input.json'
+    json_dir = ks_dir
+
+    # KS version
+    KS2ver = get_ks_version(sorting_condition)
+    
+    # Params
+    kwargs_dict = {k: v for d in params for k, v in d.items()}
+    input_json = createInputJson(
+        str(cfg_path),
+        # Directories and data
+        input_meta_path=str(metapath),
+        continuous_file=str(binpath),  # CatGT (unused)
+        npx_directory=str(binpath.parents[3]),  # CatGT (unused)
+        extracted_data_directory=str(binpath.parents[3]),  # CatGT (unused)
+        spikeGLX_data=True,
+        kilosort_output_directory=str(ks_dir),
+        ks_make_copy=False,
+        KS2ver=KS2ver,
+        **kwargs_dict
+   )   
+
+#     # rerun existing
+#     if (output_dir/'spike_times.npy').exists() and not rerun_existing:
+#         print(f'Passing: output directory is done: {output_dir}\n\n')
+#         return
+
+    print('running')
+    start = datetime.now()
+    
+    for module in modules:
+        output_json = json_dir/(f"{module}-output.json")  
+        command = (
+            f"python -W ignore -m ecephys_spike_sorting.modules.{module}"
+            f" --input_json {cfg_path}"
+            f" --output_json {output_json}"
+        )
+        print(f'Running command `{command}`')
+        subprocess.check_call(command.split(' '))
+        # !{command}
+
+    end = datetime.now()
+    print(f"{end.strftime('%H:%M:%S')}: Finished {subject}, {condition}, {sorting_condition}.")
+    print(f"Run time = {str(end - start)}\n")
+
+    return 1
+
+
+def copy_ks_dir(src, tgt):
+    "Copy src to tgt and symlink .dat files."
+    import os, shutil
+    IGNORE = ['.phy']
+    ks_dir = Path(src)
+    copy_dir = Path(tgt)
+    if copy_dir.exists():
+        print(f"Removing ks dir at {copy_dir}..")
+        shutil.rmtree(copy_dir)
+    print(f"Copying original ks dir to {copy_dir}\n")
+    os.mkdir(copy_dir)
+    for file in [f for f in ks_dir.iterdir()]:
+        if file.suffix == '.dat':
+            # Symlink all data files
+            dest = copy_dir/file.name
+            os.symlink(Path('..')/file.parent/file.name, dest)
+        else:
+            if file.name in IGNORE:
+                continue
+            # Don't use copytree because chmod raises PermissionError on smb share
+            shutil.copyfile(file, copy_dir/file.name)
+
+
+def get_ks_version(sorting_condition):
+    sorter_name, _ = parameters.get_analysis_params(
+        'sorting',
+        sorting_condition 
+    )
+    if sorter_name == 'kilosort2_5':
+        return '2.5'
+    elif sorter_name == 'kilosort2':
+        return '2.0'
+    elif sorter_name == 'kilosort3':
+        return '3.0'
+    else:
+        assert False
---
+++ ecephys_analyses/sorting/sorting.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+from pathlib import Path
+
+import spikeinterface.extractors as se
+import spikeinterface.sorters as ss
+from ecephys_analyses.data import parameters, paths
+
+
+def run_sorting(subject, condition, sorting_condition,
+                catgt_data=True, bad_channels=None, rerun_existing=True,
+                dry_run=False, clean_dat_file=False):
+    print(f"Run: {locals()}")
+
+    # Output
+    output_dir = paths.get_datapath(
+        subject,
+        condition,
+        sorting_condition,
+    )
+    print(f"Saving sorting output at {output_dir}")
+    
+    # rerun existing
+    if (output_dir/'spike_times.npy').exists() and not rerun_existing:
+        print(f'Passing: output directory is done: {output_dir}\n\n')
+        return
+
+    # Sorter parameters
+    sorter, params = parameters.get_analysis_params(
+        'sorting',
+        sorting_condition
+    )
+    
+    # Recording
+    rec = prepare_data(
+        subject, 
+        condition,
+        catgt_data=catgt_data,
+        bad_channels=bad_channels
+    )
+    
+
+    start = datetime.now()
+    if dry_run:
+        print("Dry run: doing nothing")
+    else:
+        print('Running...', end='')
+        output_dir.mkdir(exist_ok=True, parents=True)
+        ss.run_sorter(
+            sorter,
+            rec,
+            output_folder=output_dir,
+            verbose=True,
+            **params
+        )
+        rec_path = output_dir/'recording.dat'
+        if clean_dat_file and rec_path.exists():
+            rec_path.unlink()
+
+    end = datetime.now()
+    print(f"{end.strftime('%H:%M:%S')}: Finished {subject}, {condition}, {sorting_condition}.")
+    print(f"Run time = {str(end - start)}\n")
+
+    return 1
+
+
+def prepare_data(subject, condition, catgt_data=True, bad_channels=None):
+
+    binpaths = paths.get_sglx_style_datapaths(
+        subject,
+        condition,
+        'ap.bin',
+        catgt_data=catgt_data
+    )
+    
+    rec_extractors = [
+        se.SpikeGLXRecordingExtractor(binpath)
+        for binpath in binpaths
+    ]
+    if len(rec_extractors) == 1:
+        recording = rec_extractors[0]
+    else:    
+        recording = se.MultiRecordingTimeExtractor(rec_extractors)
+    total_t = recording.get_num_frames() / recording.get_sampling_frequency()
+    n_chans = recording.get_num_channels()
+    print( f'binpaths: {binpaths}')
+    print(
+        f'Recording extractor: N={len(binpaths)} bin files,'
+        f' N={n_chans}chans, T={total_t/3600}h'
+    )
+
+    assign_locations(recording, binpaths[0])
+
+    if bad_channels is not None:
+        recording = st.preprocessing.remove_bad_channels(
+            recording, bad_channel_ids=bad_channels
+        )
+
+    return recording
+
+
+def assign_locations(recording, binpath, plot=False):
+    from ecephys.sglx_utils import get_xy_coords
+    idx, x, y = get_xy_coords(binpath)
+    recording.set_channel_locations(
+        [(x[i], y[i]) for i in range(len(idx))],
+        channel_ids=idx,
+    )
+    if plot:
+        from ecephys.plot import plot_channel_coords
+        plot_channel_coords(range(len(x)), x, y)
