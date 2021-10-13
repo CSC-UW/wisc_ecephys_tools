573130762fc9712d6813910a1a55bcce9a590a29
Tom Bugnon <tombugnon@hotmail.com>
2021-10-13 13:46:19 -0500

    Refactor psth.py and add make_psth_hist()

--- ecephys_analyses/psth/psth.py
+++ ecephys_analyses/psth/psth.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import spikeextractors as se
-from ecephys.plot import plot_psth_heatmap
+from ecephys.plot import plot_psth_heatmap, plot_psth_hist
 from ecephys.units import get_selection_intervals_str
 from ecephys.units.psth import get_normed_data
 from ecephys_analyses.data import channel_groups, parameters, paths
@@ -17,7 +17,146 @@ BINSIZE_DF = 15
 PSTH_WINDOW_DF = [-5000, 2000]
 
 
-def make_psth_figures(
+def get_data(
+    subject, condition, sorting_condition,
+    good_only=False, region='all', selection_intervals=None,
+    state=None,
+    root_key=None,
+):
+    # Spikes
+    extr, info = get_sorting_data(
+        subject,
+        condition,
+        sorting_condition,
+        region=region,
+        selection_intervals=selection_intervals,
+        good_only=good_only,
+        root_key=root_key,
+    )
+    cluster_ids = extr.get_unit_ids()
+    spike_times_list = ecephys.units.get_spike_times_list(extr, cluster_ids)
+
+    # events
+    event_df = load_event_times(subject, condition, state=state, root_key=root_key)
+
+    return spike_times_list, cluster_ids, info, event_df
+
+
+def get_output_dir(subject, condition, sorting_condition, root_key=None):
+    # Save in condition dir
+    return paths.get_datapath(
+        subject,
+        condition,
+        'plots',
+        root_key=root_key
+    )/sorting_condition
+
+
+def make_pooled_psth_hist(
+    subject, condition, sorting_condition,
+    good_only=False, region='all', selection_intervals=None,
+    normalize='baseline_zscore', 
+    binsize=None, norm_window=None, plot_window=None,
+    state=None, ylim=None,
+    root_key=None,
+    save=False, show=True, output_dir=None,
+):
+    if output_dir is None:
+        output_dir = get_output_dir(subject, condition, sorting_condition, root_key=root_key)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    print(
+        f"\n\nGenerate figures for {subject} {condition}, {state}, "
+        f"{region}, good={good_only}, ylim={ylim}, output={output_dir}",
+    )
+    
+    spike_times_list, _, info, event_df = get_data(
+        subject, condition, sorting_condition,
+        good_only=good_only, region=region, selection_intervals=selection_intervals,
+        state=state,
+        root_key=root_key,
+    )
+
+    if not len(info):
+        import warnings
+        warnings.warn("N=0 selected clusters. Passing dataset.")
+        return None, None
+    if not len(event_df):
+        import warnings
+        warnings.warn("N=0 selected events. Passing dataset.")
+        return None, None
+
+    # Combine all spikes
+    pooled_spike_times = ecephys.units.pool_spike_times_list(spike_times_list)
+    pooled_cluster_ids = ['MUA']
+
+    # PSTH with "norm window" (longer baseline used for normalization)
+    _, all_psth = get_all_psth_data(
+        [pooled_spike_times], pooled_cluster_ids, event_df,
+        binsize=binsize, window=norm_window,
+    )
+    print("Done getting psth data")
+    norm_window = all_psth['window']
+    binsize = all_psth['binsize']
+    
+    # Normalize psth
+    assert all_psth['conditions'] is None
+    psth_raw = all_psth['data'][0]
+    if not len(psth_raw):
+        print("No data in psth. Passing")
+        return
+    psth_normed = get_normed_data(
+        psth_raw, normalize=normalize,
+        window=norm_window, binsize=binsize
+    )
+
+    # Subwindow for plotting
+    if plot_window is None:
+        plot_window = norm_window
+    assert plot_window[0] >= norm_window[0]
+    assert plot_window[1] < norm_window[1]
+    xvalues = np.arange(norm_window[0], norm_window[1] + 1, binsize)
+    plot_i = [i for i, x in enumerate(xvalues) if x >= plot_window[0] and x < plot_window[1]]
+    psth_array = psth_normed[:, plot_i]
+    window = plot_window
+
+    assert psth_array.shape[0] == 1
+    psth_array = psth_array[0, :]
+    fig, ax = plot_psth_hist(
+        psth_array, window, binsize, ylim=ylim,
+    )
+
+    # Title
+    title=f"PSTH: {subject}, {condition}\n"
+    if state is not None:
+        title+=f"State={state}; "
+    if region not in [None, 'all']:
+        title+=f'Region={region}; '
+    title += f'N={len(event_df)} pulses; N={psth_array.shape[0]} clusters'
+    plt.title(title)
+    #
+
+    if normalize == 'baseline_zscore':
+        ylabel = 'Z-scored firing rate'
+    elif normalize == 'baseline_norm':
+        ylabel = 'Normalized firing rate'
+    else:
+        assert False
+    plt.ylabel(ylabel)
+    
+    if show:
+        plt.show()
+    
+    if save:
+        filename = f"psth_heatmap_{subject}_{condition}_{state}_region={region}_goodonly={good_only}_norm={normalize}_ylim={ylim}_select={get_selection_intervals_str(selection_intervals)}"
+        print(f'save at {Path(output_dir)/filename}')
+        fig.savefig(Path(output_dir)/(filename + '.png'))
+        fig.savefig(Path(output_dir)/(filename + '.svg'))
+    
+    return fig, ax
+
+
+def make_psth_heatmap(
     subject, condition, sorting_condition,
     good_only=False, region='all', selection_intervals=None,
     normalize='baseline_zscore', 
@@ -25,16 +164,10 @@ def make_psth_figures(
     state=None, clim=None,
     root_key=None,
     save=False, show=True, output_dir=None,
-    draw_region_limits=True,
+    draw_region_limits=True, draw_region_suffix=None,
 ):
     if output_dir is None:
-        # Save in condition dir
-        output_dir = paths.get_datapath(
-            subject,
-            condition,
-            'plots',
-            root_key=root_key
-        )/sorting_condition
+        output_dir = get_output_dir(subject, condition, sorting_condition, root_key=root_key)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     print(
@@ -42,13 +175,26 @@ def make_psth_figures(
         f"{region}, good={good_only}, clim={clim}, output={output_dir}",
     )
     
-    # PSTH with "norm window" (longer baseline used for normalization)
-    pop, all_psth, info, event_df = get_all_psth_data(
+    spike_times_list, cluster_ids, info, event_df = get_data(
         subject, condition, sorting_condition,
-        region=region, good_only=good_only,
-        selection_intervals=selection_intervals, state=state, 
-        binsize=binsize, window=norm_window,
+        good_only=good_only, region=region, selection_intervals=selection_intervals,
+        state=state,
         root_key=root_key,
+    )
+
+    if not len(info):
+        import warnings
+        warnings.warn("N=0 selected clusters. Passing dataset.")
+        return None, None
+    if not len(event_df):
+        import warnings
+        warnings.warn("N=0 selected events. Passing dataset.")
+        return None, None
+
+    # PSTH with "norm window" (longer baseline used for normalization)
+    _, all_psth = get_all_psth_data(
+        spike_times_list, cluster_ids, event_df,
+        binsize=binsize, window=norm_window,
     )
     print("Done getting psth data")
     norm_window = all_psth['window']
@@ -76,8 +222,8 @@ def make_psth_figures(
         plot_window = norm_window
     assert plot_window[0] >= norm_window[0]
     assert plot_window[1] <= norm_window[1]
-    xvalues = np.arange(norm_window[0], norm_window[1], binsize)
-    plot_i = [i for i, x in enumerate(xvalues) if x >= plot_window[0] and x <= plot_window[1]]
+    xvalues = np.arange(norm_window[0], norm_window[1] + binsize, binsize)
+    plot_i = [i for i, x in enumerate(xvalues) if x >= plot_window[0] and x < plot_window[1]]
     psth_array = psth_array[:, plot_i]
     window = plot_window
     
@@ -104,19 +250,27 @@ def make_psth_figures(
         for i, lbl in enumerate(ylabels[::-1])
     ])[::-1]
 
+    if normalize == 'baseline_zscore':
+        cbar_label = 'Average rate (Z-scored)'
+    elif normalize == 'baseline_norm':
+        cbar_label = 'Normalized rate'
+    else:
+        assert False
+
     fig, axes = plot_psth_heatmap(
         psth_array, ylabels, window, binsize, clim=clim,
-        cbar_label='Average rate (Z-scored)'
+        cbar_label=cbar_label
     )
 
     if draw_region_limits and region=='all':
-        # Draw an horizontal line for each of the regions
+        # Draw an horizontal line for each of the regions ending with draw_region_suffix
         from ecephys.utils import find_nearest
         region_indices = {
             region: [
                 find_nearest(depths, region_lims[0], tie_select='last'),
                 find_nearest(depths, region_lims[1], tie_select='first'),
             ] for region, region_lims in region_depths.items()
+            if draw_region_suffix is None or region.endswith(draw_region_suffix)
         }  # {'region': [cluster_id_start, cluster_id_end]}
         for reg, (id_start, id_end) in region_indices.items():
             # Pass regions out of range
@@ -142,119 +296,50 @@ def make_psth_figures(
         plt.show()
     
     if save:
-        filename = f"psth_{subject}_{condition}_{state}_region={region}_goodonly={good_only}_norm={normalize}_clim={clim}_select={get_selection_intervals_str(selection_intervals)}"
+        filename = f"psth_heatmap_{subject}_{condition}_{state}_region={region}_goodonly={good_only}_norm={normalize}_clim={clim}_select={get_selection_intervals_str(selection_intervals)}"
         print(f'save {filename}')
         fig.savefig(Path(output_dir)/(filename + '.png'))
+        fig.savefig(Path(output_dir)/(filename + '.svg'))
     
     return fig, axes
 
 
-def get_average_psth_data(
-    subject, condition, sorting_condition,
-    region='all',
-    state=None,
-    good_only=False,
-    normalize='baseline_zscore',
-):
-
-    # Get raw psth
-    pop, raw_psth_dict, info, event_df = get_all_psth_data(
-        subject, condition, sorting_condition,
-        region=region,
-        state=state,
-        good_only=good_only,
-    )
-
-    # Get normalized psth
-    window = raw_psth_dict['window']
-    binsize = raw_psth_dict['binsize']
-    conditions = raw_psth_dict['conditions']
-    average_psth_dict = {
-        'window': window,
-        'binsize': binsize,
-        'event': raw_psth_dict['event'],
-        'conditions': conditions,
-        'n_clusters': pop.n_neurons,
-        'data': {},
-    }
-    assert set(average_psth_dict.keys()) >= set(raw_psth_dict.keys())
-
-    # 0 everywhere If no clusters:
-    if pop.n_neurons == 0:
-        average_psth_dict['data'][0] = np.zeros(
-             (len(np.arange(window[0], window[1], binsize)), )
-        )
-    else:
-        for i, key in enumerate(raw_psth_dict['data'].keys()):
-            # norms the data.
-            # orig_data = psth_dict['data'][cond_id]
-            orig_data = raw_psth_dict['data'][key]
-            normed_data = pop._get_normed_data(
-                orig_data, normalize=normalize, window=window, binsize=binsize,
-            ) # n_cluster, n_bins
-            average_data = np.mean(normed_data, 0)
-            average_psth_dict['data'][key] = average_data
-    
-    return average_psth_dict, info, event_df
-
-
 def get_all_psth_data(
-    subject, condition, sorting_condition,
-    region='all',
-    state=None,
+    spike_times_list, cluster_ids, event_df,
+    event_colname='sglx_stim_time',
     window=None,
     binsize=None,
-    selection_intervals=None,
-    good_only=False,
-    root_key=None,
 ):
     # Params
-    if selection_intervals is None:
-        selection_intervals = {}
     if window is None:
         window=PSTH_WINDOW_DF
     if binsize is None:
         binsize=BINSIZE_DF
-    print(f'Get all psth, window={window}, binsize={binsize}')
+    print(f'Get all psth, window={window}, binsize={binsize}, N events = {len(event_df)}')
 
-    extr, info = get_sorting_data(
-        subject,
-        condition,
-        sorting_condition,
-        region=region,
-        selection_intervals=selection_intervals,
-        good_only=good_only,
-        root_key=root_key,
-    )
+    assert event_colname in event_df.columns
 
     # Create popVis
-    neuron_list = initiate_neurons(extr)
+    neuron_list = initiate_neurons(spike_times_list, cluster_ids)
     pop = PopVis(neuron_list)
     
-    # event df
-    event_df = load_event_times(subject, condition, state=state, root_key=root_key)
-    print(f'N events = {len(event_df)}')
-    event_colname = 'sglx_stim_time'
-    assert event_colname in event_df.columns
-    
+    # Get psth
     all_psth = pop.get_all_psth(
         event=event_colname, df=event_df, window=window,
         binsize=binsize, plot=False
     )
-    return pop, all_psth, info, event_df
+    return pop, all_psth
 
 
-def initiate_neurons(extractor):
+def initiate_neurons(spike_times_list, cluster_ids):
+
+    assert len(spike_times_list) == len(cluster_ids)
     
     # Add neurons ordered by depth
     neuron_list = list()
-    for cluster_id in extractor.get_unit_ids():
-        spike_times = [
-            st for st
-            in extractor.frame_to_time(extractor.get_unit_spike_train(cluster_id))
-        ]
+    for i, cluster_id in enumerate(cluster_ids):
         # instantiate neuron
-        neuron = NeuroVis(spike_times, name=f"#{cluster_id}")
+        neuron = NeuroVis(spike_times_list[i], name=f"#{cluster_id}")
         neuron_list.append(neuron)
 
     return neuron_list
@@ -270,7 +355,7 @@ def load_event_times(subject, condition, state=None, filename=None, root_key=Non
     if cond_path.exists():
         path = cond_path
     else:
-        print(f"No event file at {cond_path}..", end='')
+        raise ValueError(f"No event file at {cond_path}..")
         root = paths.get_sglx_style_datapaths(
             subject, condition, 'ap.bin', root_key=root_key,
         )[0].parents[1]
@@ -281,7 +366,10 @@ def load_event_times(subject, condition, state=None, filename=None, root_key=Non
     if state is None:
         res = event_df
     else:
-        res = event_df[event_df['stim_state'] == state]
+        res = event_df.loc[event_df['stim_state'] == state, :].copy()
     if len(res) == 0:
-        raise ValueError(f"N=0 selected events for state={state} in file at {path}")
+        import warnings
+        warnings.warn(
+            f"N=0 selected events for state={state} in file at {path}"
+        )
     return res
