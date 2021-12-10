8fcb23ff1cdabd2f84074a34f45f2f2c5e20a716
Tom Bugnon <tombugnon@hotmail.com>
2021-12-10 05:20:43 -0600

    Move utils.depth_from_channel to ecephys.sglx.utils

--- ecephys_project_manager/config/utils.py
+++ ecephys_project_manager/config/utils.py
@@ -13,20 +13,3 @@ NOISE_CONTAM_PATH = os.path.join(MODULE_DIRECTORY, "noise_contaminated.yml")
 def get_noise_contam_dict():
     with open(NOISE_CONTAM_PATH, 'r') as f:
          return yaml.load(f, yaml.SafeLoader)
-
-
-def depth_from_channel(metapath, ch_indices):
-    """Phy depth (from tip of probe) from channel idx.
-    
-    Args:
-        ch_indices: 1-indexed index of channel of interest, ordered
-            by location on probe (1 is closest to tip)
-    """
-    _, _, ycoord = get_xy_coords(metapath)
-    sorted_y = sorted(ycoord)
-    for i, d in enumerate(sorted_y):
-        print(i + 1, d)
-    assert all([i > 0 for i in ch_indices]), '1-indexed pls'
-    return [
-        (idx, np.array(sorted_y)[idx - 1]) for idx in ch_indices
-    ]
