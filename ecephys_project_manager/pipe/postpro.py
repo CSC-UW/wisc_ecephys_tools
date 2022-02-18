49843062efdfcf9901e780cd48120294292a31ed
Graham Findlay <graham.b.findlay@gmail.com>
2022-02-18 15:49:26 -0600

    black formatting

--- ecephys_project_manager/pipe/postpro.py
+++ ecephys_project_manager/pipe/postpro.py
@@ -18,25 +18,32 @@ def run_postprocessing(
     analysis_name=None,
     sorting_analysis_name=None,
     rerun_existing=False,
-    dry_run=True
+    dry_run=True,
 ):
     assert all(
-        [arg is not None
-        for arg in [project, subject, experiment, alias, probe, analysis_name, sorting_analysis_name]]
+        [
+            arg is not None
+            for arg in [
+                project,
+                subject,
+                experiment,
+                alias,
+                probe,
+                analysis_name,
+                sorting_analysis_name,
+            ]
+        ]
     )
     print(f"Postprocessing: {locals()}\n")
 
     # Modules and associated parameters
     modules = []
     params = []
-    module_name_params = get_analysis_params(
-        'ks_postprocessing',
-       analysis_name 
-    )
+    module_name_params = get_analysis_params("ks_postprocessing", analysis_name)
     for d in module_name_params:
         modules += d.keys()
         params += d.values()
-    
+
     # Raw data paths (Sorting was performed on preproed data, but this is only used for sRate channel positions etc)
     raw_paths = get_ap_bin_paths(
         subject,
@@ -45,9 +52,9 @@ def run_postprocessing(
         probe=probe,
     )
     binpath = raw_paths[0]
-    metapath = binpath.parent/(binpath.stem + '.meta')
-    assert 'mean_waveforms' not in modules  # Must use catGT data otherwise
-    
+    metapath = binpath.parent / (binpath.stem + ".meta")
+    assert "mean_waveforms" not in modules  # Must use catGT data otherwise
+
     # Src ks dir
     ks_dir_src = get_sorting_output_path(
         project, subject, experiment, alias, probe, sorting_analysis_name
@@ -58,25 +65,37 @@ def run_postprocessing(
 
     # Target ks_dir
     ks_dir = get_sorting_output_path(
-        project, subject, experiment, alias, probe, f'{sorting_analysis_name}_{analysis_name}'
+        project,
+        subject,
+        experiment,
+        alias,
+        probe,
+        f"{sorting_analysis_name}_{analysis_name}",
     )
     if ks_dir.exists() and not rerun_existing:
-        print(f"rerun_existing == False and kilosort dir exists at {ks_dir}...:\n Doing nothing.\n\n")
+        print(
+            f"rerun_existing == False and kilosort dir exists at {ks_dir}...:\n Doing nothing.\n\n"
+        )
         return
     elif ks_dir.exists() and rerun_existing:
-        print(f"rerun_existing == True: Deleting post-processed kilosort dir at {ks_dir}.\n\n")
+        print(
+            f"rerun_existing == True: Deleting post-processed kilosort dir at {ks_dir}.\n\n"
+        )
         import shutil
+
         shutil.rmtree(ks_dir)
     print(f"Postprocessing results at: {ks_dir}\n")
 
     if not dry_run:
         print(f"Copying original ks dir to output dir\n")
         copy_ks_dir(ks_dir_src, ks_dir)
-    
+
     # Generate ecephys_spike_sorting config and save it in output dir
-    cfg_path = ks_dir/'postprocessing-input.json'
+    cfg_path = ks_dir / "postprocessing-input.json"
     json_dir = ks_dir
-    kwargs_dict = {k: v for d in params for k, v in d.items()}  # params for all modules in single dict
+    kwargs_dict = {
+        k: v for d in params for k, v in d.items()
+    }  # params for all modules in single dict
     KS2ver = get_ks_version(sorting_analysis_name)
     if not dry_run:
         _ = createInputJson(
@@ -90,29 +109,31 @@ def run_postprocessing(
             kilosort_output_directory=str(ks_dir),
             ks_make_copy=False,
             KS2ver=KS2ver,
-            **kwargs_dict
-    )   
+            **kwargs_dict,
+        )
 
     start = datetime.now()
-    
+
     for module in modules:
-        output_json = json_dir/(f"{module}-output.json")  
+        output_json = json_dir / (f"{module}-output.json")
         command = (
             f"python -W ignore -m ecephys_spike_sorting.modules.{module}"
             f" --input_json {cfg_path}"
             f" --output_json {output_json}"
         )
-        print(f'Run module {module}:')
+        print(f"Run module {module}:")
         if dry_run:
-            print(f'Dry run. Not running: {command}')
+            print(f"Dry run. Not running: {command}")
         else:
-            print(f'Running command `{command}`')
-            subprocess.check_call(command.split(' '))
+            print(f"Running command `{command}`")
+            subprocess.check_call(command.split(" "))
         # !{command}
-        print('\n')
+        print("\n")
 
     end = datetime.now()
-    print(f"{end.strftime('%H:%M:%S')}: Finished {project}, {subject}, {experiment}, {alias}, {probe}.")
+    print(
+        f"{end.strftime('%H:%M:%S')}: Finished {project}, {subject}, {experiment}, {alias}, {probe}."
+    )
     print(f"Run time = {str(end - start)}\n\n")
 
     return 1
@@ -121,44 +142,44 @@ def run_postprocessing(
 def copy_ks_dir(src, tgt):
     "Copy src to tgt and symlink .dat files."
     import os
-    IGNORE = ['.phy']
+
+    IGNORE = [".phy"]
     ks_dir = Path(src)
     copy_dir = Path(tgt)
     assert not copy_dir.exists()
     os.mkdir(copy_dir)
     for file in [f for f in ks_dir.iterdir()]:
-        if file.suffix == '.dat':
+        if file.suffix == ".dat":
             # Symlink all data files
-            dest = copy_dir/file.name
-            os.symlink(Path('..')/file.parent/file.name, dest)
+            dest = copy_dir / file.name
+            os.symlink(Path("..") / file.parent / file.name, dest)
+            pass
         else:
             if file.name in IGNORE:
                 continue
             # Don't use copytree because chmod raises PermissionError on smb share
             from ecephys.utils import system_copy
-            system_copy(file, copy_dir/file.name)
+
+            system_copy(file, copy_dir / file.name)
 
 
 def get_ks_version(sorting_analysis_name):
     try:
-        sorter_name, _ = get_analysis_params(
-            'sorting',
-            sorting_analysis_name 
-        )
-        if sorter_name == 'kilosort2_5':
-            return '2.5'
-        elif sorter_name == 'kilosort2':
-            return '2.0'
-        elif sorter_name == 'kilosort3':
-            return '3.0'
+        sorter_name, _ = get_analysis_params("sorting", sorting_analysis_name)
+        if sorter_name == "kilosort2_5":
+            return "2.5"
+        elif sorter_name == "kilosort2":
+            return "2.0"
+        elif sorter_name == "kilosort3":
+            return "3.0"
         else:
             assert False
     except ValueError:  # Key not found in analysis_cfg.yml
-        if 'ks2_5' in sorting_analysis_name:
-            return '2.5'
-        elif 'ks3_' in sorting_analysis_name:
-            return '3.0'
-        elif 'ks2_' in sorting_analysis_name:
-            return '2.0'
+        if "ks2_5" in sorting_analysis_name:
+            return "2.5"
+        elif "ks3_" in sorting_analysis_name:
+            return "3.0"
+        elif "ks2_" in sorting_analysis_name:
+            return "2.0"
         else:
             assert False
