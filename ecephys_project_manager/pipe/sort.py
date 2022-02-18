699de70199fac008b74201262ed50d6a7e13d1e0
Graham Findlay <graham.b.findlay@gmail.com>
2022-02-18 11:14:17 -0600

    Update to use new spikeinterface API, and to use yaml-specified KS2.5 install path

--- ecephys_project_manager/pipe/sort.py
+++ ecephys_project_manager/pipe/sort.py
@@ -17,14 +17,17 @@ def get_sorting_output_path(
     probe=None,
     analysis_name=None,
 ):
-    """Return f'project_dir/exp/alias/subject/{analysis_name}.{probe}' """
+    """Return f'project_dir/exp/alias/subject/{analysis_name}.{probe}'"""
     output_dirname = f"{analysis_name}.{probe}"
-    return get_alias_subject_directory(
-        project, 
-        experiment,
-        alias,
-        subject,
-    ) / output_dirname
+    return (
+        get_alias_subject_directory(
+            project,
+            experiment,
+            alias,
+            subject,
+        )
+        / output_dirname
+    )
 
 
 def run_sorting(
@@ -38,10 +41,10 @@ def run_sorting(
     prepro_project=CATGT_PROJECT_NAME,
     bad_channels=None,  # TODO
     rerun_existing=False,
-    dry_run=True
+    dry_run=True,
 ):
     """Run sorting in alias-subject directory.
-    
+
     Parameters:
     -----------
     project, subject, experiment, alias, probe: str
@@ -53,14 +56,16 @@ def run_sorting(
         Project where the preprocessed data was saved
     """
     assert all(
-        [arg is not None
-        for arg in [project, subject, experiment, alias, probe, analysis_name]]
+        [
+            arg is not None
+            for arg in [project, subject, experiment, alias, probe, analysis_name]
+        ]
     )
     print(f"Run: {locals()}")
 
     # Output
     output_dir = get_sorting_output_path(
-        project, 
+        project,
         subject,
         experiment,
         alias,
@@ -68,23 +73,24 @@ def run_sorting(
         analysis_name,
     )
     print(f"Saving sorting output at {output_dir}")
-    
+
     # rerun existing
-    if (output_dir/'spike_times.npy').exists() and not rerun_existing:
-        print(f'Passing: output directory is already done: {output_dir}\n\n')
+    if (output_dir / "spike_times.npy").exists() and not rerun_existing:
+        print(f"Passing: output directory is already done: {output_dir}\n\n")
         return True
     elif rerun_existing:
         if output_dir.exists():
-            print(f'rerun_exising==True : rm directory at {output_dir}')
+            print(f"rerun_exising==True : rm directory at {output_dir}")
             import shutil
+
             shutil.rmtree(output_dir)
 
     # Sorter parameters
     sorter, params = get_analysis_params(
-        'sorting',
+        "sorting",
         analysis_name,
     )
-    
+
     # Recording
     if not dry_run:
         rec = prepare_data(
@@ -96,32 +102,29 @@ def run_sorting(
             prepro_project=prepro_project,
             bad_channels=bad_channels,
         )
-    
 
     if dry_run:
         print("Dry run: doing nothing")
         return True
 
-    print('Running...', end='')
+    print("Running...", end="")
     start = datetime.now()
     output_dir.mkdir(exist_ok=True, parents=True)
-    ss.run_sorter(
-        sorter,
-        rec,
-        output_folder=output_dir,
-        verbose=True,
-        **params
-    )
+    if sorter == "kilosort2_5":
+        ss.sorter_dict[sorter].set_kilosort2_5_path(params["ks_path"])
+    ss.run_sorter(sorter, rec, output_folder=output_dir, verbose=True, **params)
     # Clear `recording.dat`
-    rec_path = output_dir/'recording.dat'
+    rec_path = output_dir / "recording.dat"
     rec_path.unlink()
 
     end = datetime.now()
-    print(f"{end.strftime('%H:%M:%S')}: Finished {project}, {subject}, {experiment}, {alias}, {probe}.")
+    print(
+        f"{end.strftime('%H:%M:%S')}: Finished {project}, {subject}, {experiment}, {alias}, {probe}."
+    )
     print(f"Run time = {str(end - start)}\n")
 
     # Sorting finished if we find spike_times.npy
-    return (output_dir/'spike_times.npy').exists()
+    return (output_dir / "spike_times.npy").exists()
 
 
 def prepare_data(
@@ -131,8 +134,8 @@ def prepare_data(
     probe=None,
     prepro_analysis_name=None,
     prepro_project=CATGT_PROJECT_NAME,
-    bad_channels=None
-    ):
+    bad_channels=None,
+):
 
     if prepro_analysis_name is None:
         raise NotImplementedError(
@@ -146,27 +149,29 @@ def prepare_data(
         experiment=experiment,
         alias=alias,
         probe=probe,
-        analysis_name=prepro_analysis_name
+        analysis_name=prepro_analysis_name,
     )
     metapaths, binpaths = zip(*meta_bin_paths)
 
     for m in metapaths:
         assert m.exists()  # Check that catgt finished. Should be unnecessary.
-    
+
     rec_extractors = [
-        se.SpikeGLXRecordingExtractor(binpath)
+        se.SpikeGLXRecordingExtractor(
+            binpath.parent, stream_id="".join(binpath.suffixes[:-1]).strip(".")
+        )
         for binpath in binpaths
     ]
 
     if len(rec_extractors) == 1:
         recording = rec_extractors[0]
-    else:    
+    else:
         recording = se.MultiRecordingTimeExtractor(rec_extractors)
     total_t = recording.get_num_frames() / recording.get_sampling_frequency()
     n_chans = recording.get_num_channels()
     print(
-        f'Recording extractor: Concatenate N={len(binpaths)} bin files,'
-        f' N={n_chans}chans, T={total_t/3600}h'
+        f"Recording extractor: Concatenate N={len(binpaths)} bin files,"
+        f" N={n_chans}chans, T={total_t/3600}h"
     )
 
     assign_locations(recording, binpaths[0])
@@ -182,11 +187,15 @@ def prepare_data(
 
 def assign_locations(recording, binpath, plot=False):
     from ecephys.sglx import get_xy_coords
+
     idx, x, y = get_xy_coords(binpath)
+
+    assert "#SY0" in recording.channel_ids[-1], "Expected to find SYNC channel."
+
     recording.set_channel_locations(
-        [(x[i], y[i]) for i in range(len(idx))],
-        channel_ids=idx,
+        [(x[i], y[i]) for i in range(len(idx))], channel_ids=recording.channel_ids[:-1]
     )
     if plot:
         from ecephys.plot import plot_channel_coords
+
         plot_channel_coords(range(len(x)), x, y)
