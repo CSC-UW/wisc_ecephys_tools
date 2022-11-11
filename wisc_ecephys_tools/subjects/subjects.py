7ab65029d0924449466e7f4aea320dacaca38830
Graham Findlay <graham.b.findlay@gmail.com>
2022-11-10 19:35:22 -0600

    Add new wne cache

--- wisc_ecephys_tools/subjects/subjects.py
+++ wisc_ecephys_tools/subjects/subjects.py
@@ -1,9 +1,8 @@
 import os
-import yaml
-import numpy as np
+from pathlib import Path
 
 DEFAULT_SUBJECTS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_subjects_directory():
-    return DEFAULT_SUBJECTS_DIRECTORY
+    return Path(DEFAULT_SUBJECTS_DIRECTORY)
