50b68c230aac697f4e2acd2327bc7e719fd069cb
Graham Findlay <graham.b.findlay@gmail.com>
2023-04-27 12:53:32 -0500

    Launch within xterm for progress bars, debugging, etc.

--- ephyviewer-env/launch_raster.sh
+++ ephyviewer-env/launch_raster.sh
@@ -2,4 +2,4 @@
 source /home/gfindlay/miniconda3/etc/profile.d/conda.sh 
 conda activate /home/gfindlay/miniconda3/envs/ephyviewer
 cd /home/gfindlay/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env
-python launch_raster.py
+xterm -maximized -e python launch_raster.py
