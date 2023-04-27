7d8b7612ad25822b68f128020358e9d3763fa610
Graham Findlay <graham.b.findlay@gmail.com>
2023-04-27 11:20:38 -0500

    Update lauch_raster.sh

--- ephyviewer-env/launch_raster.sh
+++ ephyviewer-env/launch_raster.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-/home/gfindlay/miniconda3/envs/ephyviewer/bin/python /home/gfindlay/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env/launch_raster.py
+#!/bin/bash
+source /home/gfindlay/miniconda3/etc/profile.d/conda.sh 
+conda activate /home/gfindlay/miniconda3/envs/ephyviewer
+cd /home/gfindlay/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env
+python launch_raster.py
