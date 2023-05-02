4b992a698359dd9fbd0e55c916886b07bb7aeb0c
Tom Bugnon <tombugnon@hotmail.com>
2023-05-02 11:49:06 -0500

    Plot only singleprobe rasters from GUI

--- ephyviewer-env/launch_raster.py
+++ ephyviewer-env/launch_raster.py
@@ -5,7 +5,7 @@ import tkinter as tk
 import rich
 
 from ecephys import wne
-from ecephys.wne.utils import load_multiprobe_sorting
+from ecephys.wne.utils import load_singleprobe_sorting
 import wisc_ecephys_tools as wet
 
 experiment = "novel_objects_deprivation"
@@ -30,7 +30,7 @@ has_hypnogram = {
         experiment, subject, "hypnogram.htsv"
     ).exists()
     for subject in available_sortings
-}  # TODO: This should just be checked in the load_multiprobe_sorting function....
+}  # TODO: This should just be checked in the load_multiprobe_sorting function.... TB: Yes but it would require (slowish) loading of all sortings
 has_anatomy = {
     subject: {
         probe: s3.get_experiment_subject_file(
@@ -39,7 +39,7 @@ has_anatomy = {
         for probe in probes
     }
     for subject, probes in available_sortings.items()
-}  # TODO: This should just be checked in the load_multiprobe_sorting function....
+}  # TODO: This should just be checked in the load_multiprobe_sorting function.... TB: Yes but it would require (slowish) loading of all sortings
 sortings_summary = {
     subject: {
         "probes": available_sortings[subject],
@@ -49,29 +49,30 @@ sortings_summary = {
     for subject in available_sortings
 }
 
-# Simple GUI dialog for getting the desired subject
+# Simple GUI dialog for getting the desired probe
 root = tk.Tk()
 tk.Label(root, text="Available sortings").pack()
 txt = tk.Text(root)
 txt.insert(tk.END, rich.pretty.pretty_repr(sortings_summary))
 txt.pack(anchor="w")
 v = tk.IntVar()
-available_subjects = list(available_sortings)
-for i, subj in enumerate(available_subjects):
-    tk.Radiobutton(root, text=subj, variable=v, value=i).pack(anchor="w")
+available_subject_probes = []
+for subj, prbs in available_sortings.items():
+        available_subject_probes += [(subj, prb) for prb in prbs]
+for i, (subj, prb) in enumerate(available_subject_probes):
+    tk.Radiobutton(root, text=f"{subj}, {prb}", variable=v, value=i).pack(anchor="w")
 tk.Button(text="Submit", command=root.destroy).pack()
 root.mainloop()
-selection = v.get()
-if selection == 0:  # If window was closed without making a selection
+subj_prb_i = v.get()
+if subj_prb_i == 0:  # If window was closed without making a selection
     sys.exit()
-subject = available_subjects[selection]
+subject, probe = available_subject_probes[subj_prb_i]
 
 # Load the sorting
 wneSubject = wne.sglx.SubjectLibrary(wet.get_subjects_directory()).get_subject(subject)
-probes = available_sortings[subject]
-sortings = {prb: "sorting" for prb in probes}
-postprocessings = {prb: "postpro" for prb in probes}
-default_filters = {
+sorting = "sorting"
+postprocessing = "postpro"
+filters = {
     "quality": {
         "good",
         "mua",
@@ -79,20 +80,22 @@ default_filters = {
     "firing_rate": (0.5, float("Inf")),
     # ...
 }
-filters = {prb: default_filters for prb in probes}
 
 wneHypnogramProject = s3 if has_hypnogram[subject] else None
-wneAnatomyProject = s3 if all(has_anatomy[subject].values()) else None
-multiprobe_sorting = load_multiprobe_sorting(
+wneAnatomyProject = s3 if has_anatomy[subject][probe] else None
+singleprobe_sorting = load_singleprobe_sorting(
     ss,
     wneSubject,
     experiment,
     alias,
-    probes,
-    sortings=sortings,
-    postprocessings=postprocessings,
+    probe,
+    sorting=sorting,
+    postprocessing=postprocessing,
     wneAnatomyProject=wneAnatomyProject,
     wneHypnogramProject=wneHypnogramProject,
 )
-multiprobe_sorting = multiprobe_sorting.refine_clusters(filters)
-multiprobe_sorting.plot_interactive_ephyviewer_raster(by="depth")
+singleprobe_sorting = singleprobe_sorting.refine_clusters(
+    filters,
+    include_nans=True,
+)
+singleprobe_sorting.plot_interactive_ephyviewer_raster(by="depth")
