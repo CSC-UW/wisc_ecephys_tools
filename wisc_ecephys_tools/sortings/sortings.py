fcd52197d8d76b36d913d31d52f129175bfdd015
Tom Bugnon <tombugnon@hotmail.com>
2023-06-08 11:10:20 -0500

    Utility for atlas-based subsetting

--- wisc_ecephys_tools/sortings/sortings.py
+++ wisc_ecephys_tools/sortings/sortings.py
@@ -1,9 +1,26 @@
 import wisc_ecephys_tools as wet
+from typing import Union
 from ecephys.utils import read_htsv
+from pathlib import Path
+
+from bg_atlasapi.bg_atlas import BrainGlobeAtlas
+
+SHARED_PROJECT_NAME = "shared_s3"
+PROJ = wet.get_wne_project(SHARED_PROJECT_NAME)
+BRAINGLOBE_DIR = PROJ.get_project_directory() / ".brainglobe"
+DF_ATLAS = "whs_sd_rat_39um"
+
+DF_EXCLUDE_DESCENDANTS_OF = ["V", "wmt"]
+
+Pathlike = Union[Path, str]
 
 
-def get_completed_subject_probes(experiment, alias):
-    """Return list of (<subj>, <prb>) for sortings with everything"""
+def get_atlas(atlas_name: str = DF_ATLAS, brainglobe_dir: Pathlike = BRAINGLOBE_DIR):
+    return BrainGlobeAtlas(atlas_name, brainglobe_dir=brainglobe_dir)
+
+
+def get_subject_probe_list(experiment: str, alias: str) -> list[tuple[str, str]]:
+    """Return [(<subj>, <prb>)] list of completed sortings."""
 
     # Get the available sortings
     ss = wet.get_wne_project("shared_sortings")
@@ -12,26 +29,20 @@ def get_completed_subject_probes(experiment, alias):
     sortings_dir = ss.get_alias_directory(experiment, alias)
 
     available_sortings = {
-        subj.name: [
-            x.name.removeprefix("sorting.") for x in sorted(subj.glob("sorting.imec*"))
-        ]
+        subj.name: [x.name.removeprefix("sorting.") for x in sorted(subj.glob("sorting.imec*"))]
         for subj in sortings_dir.iterdir()
         if subj.is_dir()
     }  # e.g. {'CNPIX4-Doppio': ['imec0', 'imec1], 'CNPIX9-Luigi: ['imec0], ...}
 
     # Collect info about the various sortings, for display, and for determing parameters to use when loading
     has_hypnogram = {
-        subject: s3.get_experiment_subject_file(
-            experiment, subject, "hypnogram.htsv"
-        ).exists()
+        subject: s3.get_experiment_subject_file(experiment, subject, "hypnogram.htsv").exists()
         for subject in available_sortings
     }  # TODO: This should just be checked in the load_multiprobe_sorting function.... TB: Yes but it would require (slowish) loading of all sortings
 
     has_anatomy = {
         subject: {
-            probe: s3.get_experiment_subject_file(
-                experiment, subject, f"{probe}.structures.htsv"
-            ).exists()
+            probe: s3.get_experiment_subject_file(experiment, subject, f"{probe}.structures.htsv").exists()
             for probe in probes
         }
         for subject, probes in available_sortings.items()
@@ -42,40 +53,56 @@ def get_completed_subject_probes(experiment, alias):
         available_subject_probes += [(subj, prb) for prb in prbs]
 
     completed_subject_probes = [
-        (subj, prb) for subj, prb in available_subject_probes
-        if has_hypnogram[subj]
-        and has_anatomy[subj][prb]
+        (subj, prb) for subj, prb in available_subject_probes if has_hypnogram[subj] and has_anatomy[subj][prb]
     ]
 
     return completed_subject_probes
 
 
-def get_completed_subject_probe_structures(experiment, alias, acronyms_to_ignore=None, acronyms_to_include=None):
+def get_subject_probe_structure_list(
+    experiment: str,
+    alias: str,
+    select_descendants_of: list[str] = None,
+    exclude_descendants_of: list[str] = DF_EXCLUDE_DESCENDANTS_OF,
+    atlas: BrainGlobeAtlas = None,
+) -> list[tuple[str, str, str]]:
+    """Return [(<subj>, <prb>, <acronym>)] list of structures of interest."""
 
-    if acronyms_to_ignore is None:
-        acronyms_to_ignore = []
+    if atlas is None:
+        atlas = get_atlas()
+
+    if select_descendants_of is not None:
+        unrecognized = [a for a in select_descendants_of if not a in atlas.lookup_df.acronym.values]
+        if any(unrecognized):
+            raise ValueError(f"Unrecognized acronyms in `select_descendants_of` kwarg: {unrecognized}.")
+    if exclude_descendants_of is not None:
+        unrecognized = [a for a in exclude_descendants_of if not a in atlas.lookup_df.acronym.values]
+        if any(unrecognized):
+            raise ValueError(f"Unrecognized acronyms in `exclude_descandants_of` kwarg: {unrecognized}.")
 
     # Get the available sortings
     s3 = wet.get_wne_project("shared_s3")
 
-    completed_subject_probes = get_completed_subject_probes(
+    completed_subject_probes = get_subject_probe_list(
         experiment,
         alias,
     )
 
     completed_subject_probe_structures = []
     for subj, prb in completed_subject_probes:
-        struct = read_htsv(
-            s3.get_experiment_subject_file(
-                experiment, subj, f"{prb}.structures.htsv"
-            )
-        )
+        struct = read_htsv(s3.get_experiment_subject_file(experiment, subj, f"{prb}.structures.htsv"))
         for acronym in struct.acronym.unique():
-            if acronyms_to_include is not None and acronym not in acronyms_to_include:
-                continue
-            if not acronym in acronyms_to_ignore:
-                completed_subject_probe_structures.append(
-                    (subj, prb, acronym)
-                )
-    
+            ancestors = atlas.get_structure_ancestors(acronym)
+            if exclude_descendants_of is not None:
+                if acronym in exclude_descendants_of or any(
+                    [exclude in ancestors for exclude in exclude_descendants_of]
+                ):
+                    continue
+            if select_descendants_of is not None:
+                if acronym not in select_descendants_of and not any(
+                    [select in ancestors for select in select_descendants_of]
+                ):
+                    continue
+            completed_subject_probe_structures.append((subj, prb, acronym))
+
     return completed_subject_probe_structures
