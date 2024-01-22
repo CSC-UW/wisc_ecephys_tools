1107f5c5ff39883c99bb257b536e8a58f61d0eb2
Graham Findlay <grahamfindlay@users.noreply.github.com>
2024-01-22 14:04:06 -0600

    Update launch_raster.sh

--- ephyviewer-env/launch_raster.sh
+++ ephyviewer-env/launch_raster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh 
-conda activate ~/miniconda3/envs/ephyviewer
+source ~/miniforge3/etc/profile.d/conda.sh 
+conda activate ~/miniforge3/envs/ephyviewer
 cd ~/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env
 xterm -maximized -e python launch_raster.py
