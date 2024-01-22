0c7c18c8dd501f9dd4f7144734adf40afe76b078
Graham Findlay <grahamfindlay@users.noreply.github.com>
2024-01-22 13:17:32 -0600

    Update launch_raster.sh
    Use generic home directory instead of Graham's

--- ephyviewer-env/launch_raster.sh
+++ ephyviewer-env/launch_raster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/gfindlay/miniconda3/etc/profile.d/conda.sh 
-conda activate /home/gfindlay/miniconda3/envs/ephyviewer
-cd /home/gfindlay/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env
+source ~/miniconda3/etc/profile.d/conda.sh 
+conda activate ~/miniconda3/envs/ephyviewer
+cd ~/projects/ephyviewer/wisc_ecephys_tools/ephyviewer-env
 xterm -maximized -e python launch_raster.py
