446b4d27e3275f5d071c06a9a236f4a5f9dee85a
Graham Findlay <graham.b.findlay@gmail.com>
2025-03-11 18:39:29 -0500

    Expose get_atlas and update brainglobe dependency

--- src/wisc_ecephys_tools/sortings/__init__.py
+++ src/wisc_ecephys_tools/sortings/__init__.py
@@ -1,3 +1,7 @@
-from .sortings import get_subject_probe_list, get_subject_probe_structure_list
+from .sortings import (
+    get_atlas,
+    get_subject_probe_list,
+    get_subject_probe_structure_list,
+)
 
-__all__ = ["get_subject_probe_list", "get_subject_probe_structure_list"]
+__all__ = ["get_atlas", "get_subject_probe_list", "get_subject_probe_structure_list"]
