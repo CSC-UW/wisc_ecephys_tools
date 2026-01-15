bed22993318c93af6b8d85880ccb3c98494ee8ee
Graham Findlay <graham.b.findlay@gmail.com>
2026-01-15 13:39:05 -0600

    Remove eager import of anything with heavy dependencies.

--- src/wisc_ecephys_tools/__init__.py
+++ src/wisc_ecephys_tools/__init__.py
@@ -1,12 +1,15 @@
-from . import constants, core, ephyviewer_app, rats
+# Only import lightweight modules at package level.
+# ephyviewer_app and rats must be imported directly to avoid slow import times
+# from GUI libraries and brainglobe:
+#   from wisc_ecephys_tools import ephyviewer_app
+#   from wisc_ecephys_tools import rats
+from . import constants, core
 from .projects import get_sglx_project, get_wne_project
 from .subjects import get_sglx_subject, get_subjects_directory
 
 __all__ = [
     "constants",
     "core",
-    "ephyviewer_app",
-    "rats",
     "get_sglx_project",
     "get_wne_project",
     "get_sglx_subject",
